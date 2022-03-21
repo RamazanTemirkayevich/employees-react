dcb96c6a0602148e8fae5174881fa77459b92e30
Family <family@MacBook-Air-Family.local>
2022-03-21 16:36:06 +0300

    first react app/data

---
+++ src/components/app/app.js
@@ -0,0 +1,32 @@
+import AppInfo from '../app-info/app-info';
+import SearchPanel from '../search-panel/search-panel';
+import AppFilter from '../app-filter/app-filter';
+import EmployeesList from '../employees-list/employees-list';
+import EmployeesAddForm from '../employees-add-form/employees-add-form';
+
+import './app.css';
+
+function App() {
+
+    const data = [
+      {name: 'Will C.', salary: 850, increase: false, id: 1},
+      {name: 'Selena B.', salary: 2000, increase: true, id: 2},
+      {name: 'Adam E.', salary: 1500, increase: false, id: 3},
+    ];
+
+    return (
+        <div className="app">
+            <AppInfo/>
+
+            <div className="search-panel">
+                <SearchPanel/>
+                <AppFilter/>
+            </div>
+
+            <EmployeesList data={data}/>
+            <EmployeesAddForm/>
+        </div>
+    )
+}
+
+export default App;
